504fa309240def0e41e393095937203ab15ad1c3
Mike Archer <mlarcher531@gmail.com>
2020-07-25 20:26:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-
+  crabble = ["wow","wa","arrays!"]
+  scrabble[2]
 end
 
 def update_element_from_index(array, index_number, element)
