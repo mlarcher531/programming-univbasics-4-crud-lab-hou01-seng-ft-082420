46e0d4f2905881b7f1420bfa3aa1853fc5a7e03d
Mike Archer <mlarcher531@gmail.com>
2020-07-25 20:16:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers = [10,20,30,40]
-  numbers << 50
+  array = [10,20,30,40]
+  element = 50
+  array << 50
 end
 
 def add_element_to_start_of_array(array, element)
