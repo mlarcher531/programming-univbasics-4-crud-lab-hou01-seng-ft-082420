e3aa461f25378fec65afc79968f574ef626cc420
Mike Archer <mlarcher531@gmail.com>
2020-07-25 20:19:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers = [10,20,30,40]
-  numbers.push 50
+  names = ["bill","ted","gary"]
+  names<< "frank"
 end
 
 def add_element_to_start_of_array(array, element)
